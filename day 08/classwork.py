da148c675b538b7a5529379e9d88f3af4a72d7c1
bera32 <berashvilibeka32@gmail.com>
2024-03-01 11:54:32 +0100

    the eighth commit

---
+++ day 08/classwork.py
@@ -0,0 +1,7 @@
+stand_up = 60
+wake_up = 100
+
+your_stand_up = int(input("please enter your stand up: "))
+your_wake_up =int(input("please enter your wake up: "))
+
+print(stand_up >= your_stand_up or wake_up <= your_wake_up)
---
+++ day 08/homework1.py
@@ -0,0 +1,6 @@
+push_up =int(input("please enter your push_up: "))
+squats = int(input("please enter yoer squats: "))
+
+
+print(push_up <= squats)
+print(push_up == squats)
